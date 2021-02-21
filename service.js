d5d825cd96cf0ad7236a6290c067cb6532cafbe3
Victor Freitas <60153670+cnfreitax@users.noreply.github.com>
2021-02-21 19:16:51 -0300

    feat: first commit

---
+++ service.js
@@ -0,0 +1,24 @@
+const https = require('https')
+
+class Service {
+  async makeRequest(url) {
+    return new Promise((resolve, reject) => {
+      https.get(url, response => {
+        response.on('data', data => resolve(JSON.parse(data)))
+        response.on('error', reject)
+      })
+    })
+  }
+
+  async getPlanets(url) {
+    const response = await this.makeRequest(url)
+    
+    return {
+      name: response.name,
+      surfaceWater: response.surface_water,
+      apperedIn: response.films.length
+    }
+  }
+}
+
+module.exports = Service
